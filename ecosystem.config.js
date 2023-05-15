bec7990c28d15fecc6dde681583c8f8a2187503a
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2023-05-15 22:58:11 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '500,',
+      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'env',
       },
